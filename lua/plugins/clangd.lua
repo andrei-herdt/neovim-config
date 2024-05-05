2f5fbeed66df8c689a8cfd7ce71a52a5c58ad145
Andrei Herdt <aherdt@tesla.com>
2024-05-05 13:59:18 -0700

    Activate clangd

---
+++ lua/plugins/clangd.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- Ensure mason installs the server
+        clangd = {
+          mason = false,
+        },
+      },
+    },
+  },
+}
