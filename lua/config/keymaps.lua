faf29a1ca315916de6baa5b87b881505a77ded5a
Andrei Herdt <aherdt@tesla.com>
2024-05-05 13:59:05 -0700

    Add keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,10 +2,11 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "[<space>", "mzO<Esc>`z", { desc = "add blank line above" })
-vim.keymap.set("n", "]<space>", "mzo<Esc>`z", { desc = "add blank line below" })
+vim.keymap.set("n", "<space>j", "mzO<Esc>`z", { desc = "add blank line above" })
+vim.keymap.set("n", "<space>k", "mzo<Esc>`z", { desc = "add blank line below" })
 vim.keymap.set("n", "<Esc><Esc>", ":w<CR>", { desc = "write file" })
 vim.keymap.set("i", "jj", "<Esc>", { desc = "escape mode" })
+vim.keymap.set("n", "<C-b><C-b>", ":!b86", { desc = "Execute b86 alias" })
 
 -- binding for Markdown Preview
 vim.keymap.set("n", "<leader>mp", "<cmd> MarkdownPreview<CR>", { desc = "Open Preview" })
