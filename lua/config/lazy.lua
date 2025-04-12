d319a60b1175a1dbed9b2f96dbcc7c2111f2e13f
Andrei Herdt <aherdt@tesla.com>
2025-04-12 13:11:39 -0700

    update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,7 +12,8 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "lazyvim.plugins.extras.ai.Copilot" },
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.ai.copilot" },
     -- { import = "lazyvim.plugins.extras.lang.clangd" },
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   -- disable trouble
   { "nvim-treesitter-context", enabled = false },
   { "folke/noice.nvim", enabled = false },
+  { "nvim-lualine/lualine.nvim", enabled = false },
 }
